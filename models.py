fc0f703407f2d378720593009ecda19471bc86bd
crazyinsan03-web <crazyinsan03@gmail.com>
2026-04-17 09:10:15 +0530

    Update models.py

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ db = SQLAlchemy()
 
 class Song(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(500), nullable=False)
-    song_url = db.Column(db.Text, nullable=False)
-    thumbnail = db.Column(db.Text, nullable=True)
+    title = db.Column(db.String(500))
+    # Ye dono 'Text' hone chahiye, 'String(200)' nahi!
+    song_url = db.Column(db.Text) 
+    thumbnail = db.Column(db.Text)
