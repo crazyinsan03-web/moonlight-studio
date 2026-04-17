ef7d8f9d2ed0a2ced14a44858350dd0cc379a743
crazyinsan03-web <crazyinsan03@gmail.com>
2026-04-17 11:59:12 +0530

    Update models.py

--- models.py
+++ models.py
@@ -9,8 +9,10 @@ class Song(db.Model):
     video_id = db.Column(db.String(100), nullable=False)
     # Thumbnail ki link lambi ho sakti hai isliye Text use kiya hai
     thumbnail = db.Column(db.Text)
-    # Optional: Agar kabhi date wise sort karna ho
-    # created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    
+    # --- NAYA COLUMN ---
+    # Har gaane ke likes count karne ke liye
+    likes = db.Column(db.Integer, default=0)
 
     def __repr__(self):
         return f'<Song {self.title}>'
